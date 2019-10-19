503509a58687da655faaebcf0cee34f2c40fc6a0
Christoph <cspeier@gmail.com>
2019-10-19 20:24:58 +0200

    Added Overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MSTracerComponent} from './mstracer/app.component.mstracer';
+import { MSTracerComponent } from './mstracer/app.component.mstracer';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MSTracerComponent
+    MSTracerComponent,
+    DashboardComponent,
+    WelcomeComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule
