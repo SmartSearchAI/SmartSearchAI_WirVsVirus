7f0e4965b6b11cd22db26d952d0e4bd65c7c8a1c
Felix Gundlack <GundlackFelixDev@gmail.com>
2019-10-20 13:14:58 +0200

    Calender IFrame to google working

---
+++ src/app/cm_calender/cm.calender.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cm-calender',
+  templateUrl: './cm.calender.component.html',
+  styleUrls: ['./cm.calender.component.scss']
+})
+
+export class CMCalenderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
