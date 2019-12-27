6c403595fd78ca60ad215c33f4e02aa3cfcdc4d2
Felix Gundlack <GundlackFelixDev@gmail.com>
2019-12-27 10:55:46 +0100

    #2 Comment out firebase Environment @TODO Udpate Firebase Connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  /* Example Firebase Connection
   firebase: {
     apiKey: 'AIzaSyCoQUDDx1GFSYPffaL8hlLnoIUQuC1Gvzo',
     authDomain: 'fightclub-20.firebaseapp.com',
@@ -8,5 +9,5 @@ export const environment = {
     storageBucket: 'fightclub-20.appspot.com',
     messagingSenderId: '628836928614',
     appId: '1:628836928614:web:fc3301e998a48a8a2131a7'
-  }
+  }*/
 };
