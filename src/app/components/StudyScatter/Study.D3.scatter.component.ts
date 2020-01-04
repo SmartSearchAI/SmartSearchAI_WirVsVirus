f69e59a256b4c72bea8e0e335bd7e2ee2f4cd535
Felix Gundlack <GundlackFelixDev@gmail.com>
2020-01-04 15:37:30 +0100

    #16 ScatterPlot Rendering Working - Data Corrupted

--- src/app/components/StudyScatter/Study.D3.scatter.component.ts
+++ src/app/components/StudyScatter/Study.D3.scatter.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input} from '@angular/core';
-import * as d3 from "d3";
+import { Component, OnInit, OnChanges, Input, SimpleChanges} from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'study-D3-scatter',
@@ -7,47 +7,84 @@ import * as d3 from "d3";
   styleUrls: ['./Study.D3.scatter.component.scss']
 })
 
-export class StudyD3ScatterComponent implements OnInit {
+export class StudyD3ScatterComponent implements OnInit, OnChanges {
   @Input() $Ids: Array<string>;
   @Input() $Data: Array<{x: number, y: number, value: number}>;
   private D3DOM: {
     id: string,
-    svg: any,
     width: number,
     height: number,
-    margin: number
+    margin: {top: number, right: number, bottom: number, left: number};
   };
   constructor() {
     this.D3DOM = {
-      id: '#StudyD3Scatter',
-      svg: null,
+      id: 'StudyD3Scatter',
       width: null,
-      height: null,
-      margin: null
+      height: null
     };
   }
 
   ngOnInit() {
-    const _id = this.D3DOM.id;
-    this.D3DOM.svg = document.getElementById(_id);
-    this.D3DOM.width = this.D3DOM.svg.offsetWidth;
-    this.D3DOM.height = this.D3DOM.svg.offsetHeight ? this.D3DOM.svg.offsetHeight : 450;
-    const width = this.D3DOM.width;
-    const height = this.D3DOM.height;
-    this.D3DOM.svg = d3.select(this.D3DOM.id)
-    .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-    .append('g')
-      .attr('transform', `translate(${width / 2}, ${height / 2})` );
-
+    // Render Initial Input
     this.updateChart();
   }
 
-  updateChart(){
-    const data = this.$Data;
-    const labels = this.$Ids;
+  ngOnChanges(changes: SimpleChanges): void {
+    this.updateChart();
+  }
+
+  updateChart() {
+    // Remove whatever chart with the same id/class was present before
+    let element = d3.select(`#${this.D3DOM.id}`);
+    element.select('svg').remove();
+    element = document.getElementById(this.D3DOM.id);
+    const margin = {top: 20, right: 20, bottom: 30, left: 40};
+    const width = (element.offsetWidth ? element.offsetWidth : 450) - margin.top - margin.bottom;
+    const height = width - margin.left - margin.right;
+
+
+    const data = this.$Ids.map((Id) => {
+      return {
+        X: Math.random().toFixed(3),
+        Y: Math.random().toFixed(3),
+        C: Math.random().toFixed(3),
+        Label: Id
+      };
+    });
+
+    // append the svg object to the body of the page
+    const svg = d3.select(`#${this.D3DOM.id}`)
+      .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+        .attr('transform',
+              'translate(' + margin.left + ',' + margin.top + ')');
+
+    // Add X axis
+    const x = d3.scaleLinear()
+      .domain([0, 1])
+      .range([ 0, width ]);
+    svg.append('g')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+      .domain([0, 1])
+      .range([ height, 0]);
+    svg.append('g')
+        .call(d3.axisLeft(y));
+
+    // Add dots
+    svg.append('g')
+      .selectAll('dot')
+      .data(data)
+      .enter()
+      .append('circle')
+        .attr('cx', function (d) { return d.X; } )
+        .attr('cy', function (d) { return d.Y; } )
+        .attr('r', 1.5)
+        .style('fill', '#69b3a2');
   }
 }
-
-
